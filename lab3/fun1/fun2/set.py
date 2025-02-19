34462a721d2e43c82dd9872ec749c40e136f009e
arman begman <armanbegman@MacBook-Air-arman.local>
2025-02-19 14:30:20 +0500

    lab4

---
+++ lab3/fun1/fun2/set.py
@@ -0,0 +1,12 @@
+a = [1,2,3,4,5]
+b = (1, 2, 3, 4, 5)
+c = {"name":"aqzhol", "age": "18", "krush":"yes"}
+v = ("Idakhan", "Aqzhol","krush")
+print(a, b, c, v)
+
+s = "idakhan aqzhol"
+b = s.upper()
+d = s.replace("  ","_")
+print(b)
+print(d)
+
--- lab4/date.md/1.py
+++ lab4/date.md/1.py
@@ -2,4 +2,3 @@ from datetime import datetime,timedelta
 today = datetime.today()
 newdate = today - timedelta(days=5)
 print(newdate.strftime("%d-%m-%Y"))
-
